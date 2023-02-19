185ad0aada74393f8313cc92ed8682bd32019d1d
Umut Sevdi <sevdiumut@gmail.com>
2023-02-19 22:03:00 +0300

    applet cleaning

--- applets/tab/launch.sh
+++ applets/tab/launch.sh
@@ -4,7 +4,7 @@
 # @author umutsevdi 
 # @requires rofi
 
-[ "$ROFI_APPLETS_PATH" = "" ] && ROFI_APPLETS_PATH=$HOME/.dotfiles/applets/
+[ "$ROFI_APPLETS_PATH" = "" ] && ROFI_APPLETS_PATH=$HOME/.dotfiles/rofi-applets/
 theme="config"
 dir="${ROFI_APPLETS_PATH}/applets/tab"
 rofi -show window  -theme $dir/"$theme" 
